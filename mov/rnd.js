30559a3179a43422ee4ad33cdbafe0c3396f6880
Aqua Minerale <20503770+windowsdelete@users.noreply.github.com>
2024-11-03 20:30:04 +0300

    Cloudflare Optimization
    ✘ [ERROR] Error: Pages only supports files up to 25 MiB in size

--- mov/rnd.js
+++ mov/rnd.js
@@ -1,10 +1,10 @@
 		var videoArray = [
-			'one.mp4',
+			//'one.mp4',
 			'two.mp4',
 			'three.mp4',
-			'four.mp4',
+			//'four.mp4',
 			'five.mp4',
-			'six.mp4',
+			//'six.mp4',
 			'seven.mp4'
 				];
 		var count = Math.floor(Math.random() * videoArray.length);
